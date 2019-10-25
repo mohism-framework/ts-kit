79cac033bf63b4c8fd4b9dfb8593e0f474111075
蓝 <lanhao@class100.com>
2019-10-25 14:46:04 +0800

    ts kit

---
+++ src/commands/tskit.action.ts
@@ -0,0 +1,73 @@
+import ActionBase from '@mohism/cli-wrapper/dist/libs/action.class';
+import { ArgvOption, Dict } from '@mohism/cli-wrapper/dist/libs/utils/type';
+import { existsSync, readFileSync, writeFileSync } from 'fs';
+import { EOL } from 'os';
+import { exec } from 'shelljs';
+
+import { DEPS_DEV, ESLINTRC, IGNORE_FILES, SCRIPTS } from '../libs/constant';
+
+import('colors');
+
+class TsKit extends ActionBase {
+  options(): Dict<ArgvOption> {
+    return {};
+  }
+
+  description(): string {
+    return 'an awesome typescript kit';
+  }
+
+  async run(): Promise<any> {
+    // 要运行kit的项目位置
+    const root: string = process.cwd();
+    if (!existsSync(`${root}/package.json`)) {
+      if (0 !== exec('npm init --init', {
+        silent: true,
+      }).code) {
+        this.fatal('Run "npm init" failed.');
+      }
+      this.info(`Successful generate: ${`${root}/package.json`.blue}`);
+    }
+    const pkg = require(`${root}/package.json`);
+    // add npm scripts
+    pkg.scripts = { ...pkg.scripts, ...SCRIPTS };
+    writeFileSync(`${root}/package.json`, JSON.stringify(pkg, null, 2));
+    this.info(`Successful update scripts: ${'npm run build, npm run lint'.blue}`);
+
+    // install deps
+    const toInstall = DEPS_DEV
+      .filter(item => !pkg.devDependencies[item])
+      .join(' ');
+    if (0 !== exec(`npm i -D ${toInstall}`, { silent: true }).code) {
+      this.fatal('Dependencies fail to installed');
+    }
+    this.info(`Successful install: ${toInstall.blue}`);
+
+    // ignore file
+    Object.keys(IGNORE_FILES).forEach(file => {
+      let ignore: Set<string> = new Set(IGNORE_FILES[file]);
+      if (existsSync(`${root}/${file}`)) {
+        ignore = new Set([
+          ...readFileSync(`${root}/${file}`).toString().split(EOL),
+          ...IGNORE_FILES[file],
+        ]);
+      }
+      writeFileSync(`${root}/${file}`, Array.from(ignore).join(EOL));
+      this.info(`Successful updated: ${`${root}/${file}`.blue}`);
+    });
+
+    // eslintrc
+    if (!existsSync(`${root}/.eslintrc.json`)) {
+      writeFileSync(`${root}/.eslintrc.json`, ESLINTRC);
+      this.info(`Successful generate: ${`${root}/.eslintrc.json`.blue}`);
+    }
+
+    // tsconfig
+    if (!existsSync(`${root}/tsconfig.json`)) {
+      writeFileSync(`${root}/tsconfig.json`, ESLINTRC);
+      this.info(`Successful generate: ${`${root}/tsconfig.json`.blue}`);
+    }
+  }
+}
+
+export default new TsKit();
---
+++ src/libs/constant.ts
@@ -0,0 +1,145 @@
+import { Dict } from '@mohism/cli-wrapper/dist/libs/utils/type';
+
+export const DEPS: Array<string> = [];
+
+export const DEPS_DEV: Array<string> = [
+  '@types/node',
+  '@typescript-eslint/eslint-plugin',
+  '@typescript-eslint/parser',
+  'eslint',
+  'ts-node',
+  'typescript'
+];
+
+export const SCRIPTS: Dict<string> = {
+  'build': 'npx eslint src/**/*.ts --fix && npx tsc',
+  'lint': 'npx eslint src/**/*.ts --fix',
+};
+
+export const GIT_IGNORE: Array<string> = [
+  'node_modules',
+  'dist',
+  'tsconfig.tsbuildinfo',
+];
+
+export const NPM_IGNORE: Array<string> = [
+  'src',
+  'node_modules',
+  'tsconfig.json',
+];
+
+export const IGNORE_FILES: Dict<Array<string>> = {
+  '.gitignore': GIT_IGNORE,
+  '.npmignore': NPM_IGNORE,
+};
+
+export const ESLINTRC: string =
+  `{
+  "env": {
+      "es6": true,
+      "node": true
+  },
+  "extends": [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/eslint-recommended"
+  ],
+  "globals": {
+      "Atomics": "readonly",
+      "SharedArrayBuffer": "readonly"
+  },
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+      "ecmaVersion": 2018,
+      "sourceType": "module"
+  },
+  "plugins": [
+      "@typescript-eslint"
+  ],
+  "rules": {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "error",
+      "indent": [
+          "error",
+          2
+      ],
+      "linebreak-style": [
+          "error",
+          "unix"
+      ],
+      "quotes": [
+          "error",
+          "single"
+      ],
+      "semi": [
+          "error",
+          "always"
+      ]
+  }
+}`;
+
+export const TSCONFIG: string =
+  `{
+  "compilerOptions": {
+    /* Basic Options */
+    "incremental": true,                   /* Enable incremental compilation */
+    "target": "ES2015", /* Specify ECMAScript target version: 'ES3' (default), 'ES5', 'ES2015', 'ES2016', 'ES2017', 'ES2018', 'ES2019' or 'ESNEXT'. */
+    "module": "commonjs", /* Specify module code generation: 'none', 'commonjs', 'amd', 'system', 'umd', 'es2015', or 'ESNext'. */
+    // "lib": [],                             /* Specify library files to be included in the compilation. */
+    // "allowJs": true,                       /* Allow javascript files to be compiled. */
+    // "checkJs": true,                       /* Report errors in .js files. */
+    // "jsx": "preserve",                     /* Specify JSX code generation: 'preserve', 'react-native', or 'react'. */
+    "declaration": true, /* Generates corresponding '.d.ts' file. */
+    // "declarationMap": true,                /* Generates a sourcemap for each corresponding '.d.ts' file. */
+    // "sourceMap": true,                     /* Generates corresponding '.map' file. */
+    // "outFile": "./",                       /* Concatenate and emit output to single file. */
+    "outDir": "./dist", /* Redirect output structure to the directory. */
+    "rootDir": "./src", /* Specify the root directory of input files. Use to control the output directory structure with --outDir. */
+    // "composite": true,                     /* Enable project compilation */
+    // "tsBuildInfoFile": "./",               /* Specify file to store incremental compilation information */
+    // "removeComments": true,                /* Do not emit comments to output. */
+    // "noEmit": true,                        /* Do not emit outputs. */
+    // "importHelpers": true,                 /* Import emit helpers from 'tslib'. */
+    // "downlevelIteration": true,            /* Provide full support for iterables in 'for-of', spread, and destructuring when targeting 'ES5' or 'ES3'. */
+    // "isolatedModules": true,               /* Transpile each file as a separate module (similar to 'ts.transpileModule'). */
+    /* Strict Type-Checking Options */
+    "strict": true, /* Enable all strict type-checking options. */
+    // "noImplicitAny": true,                 /* Raise error on expressions and declarations with an implied 'any' type. */
+    // "strictNullChecks": true,              /* Enable strict null checks. */
+    // "strictFunctionTypes": true,           /* Enable strict checking of function types. */
+    // "strictBindCallApply": true,           /* Enable strict 'bind', 'call', and 'apply' methods on functions. */
+    // "strictPropertyInitialization": true,  /* Enable strict checking of property initialization in classes. */
+    // "noImplicitThis": true,                /* Raise error on 'this' expressions with an implied 'any' type. */
+    // "alwaysStrict": true,                  /* Parse in strict mode and emit "use strict" for each source file. */
+    /* Additional Checks */
+    // "noUnusedLocals": true,                /* Report errors on unused locals. */
+    // "noUnusedParameters": true,            /* Report errors on unused parameters. */
+    // "noImplicitReturns": true,             /* Report error when not all code paths in function return a value. */
+    // "noFallthroughCasesInSwitch": true,    /* Report errors for fallthrough cases in switch statement. */
+    /* Module Resolution Options */
+    // "moduleResolution": "node",            /* Specify module resolution strategy: 'node' (Node.js) or 'classic' (TypeScript pre-1.6). */
+    // "baseUrl": "./",                       /* Base directory to resolve non-absolute module names. */
+    // "paths": {},                           /* A series of entries which re-map imports to lookup locations relative to the 'baseUrl'. */
+    // "rootDirs": [],                        /* List of root folders whose combined content represents the structure of the project at runtime. */
+    // "typeRoots": [],                       /* List of folders to include type definitions from. */
+    // "types": [],                           /* Type declaration files to be included in compilation. */
+    // "allowSyntheticDefaultImports": true,  /* Allow default imports from modules with no default export. This does not affect code emit, just typechecking. */
+    "esModuleInterop": true /* Enables emit interoperability between CommonJS and ES Modules via creation of namespace objects for all imports. Implies 'allowSyntheticDefaultImports'. */
+    // "preserveSymlinks": true,              /* Do not resolve the real path of symlinks. */
+    // "allowUmdGlobalAccess": true,          /* Allow accessing UMD globals from modules. */
+    /* Source Map Options */
+    // "sourceRoot": "",                      /* Specify the location where debugger should locate TypeScript files instead of source locations. */
+    // "mapRoot": "",                         /* Specify the location where debugger should locate map files instead of generated locations. */
+    // "inlineSourceMap": true,               /* Emit a single file with source maps instead of having a separate file. */
+    // "inlineSources": true,                 /* Emit the source alongside the sourcemaps within a single file; requires '--inlineSourceMap' or '--sourceMap' to be set. */
+    /* Experimental Options */
+    // "experimentalDecorators": true,        /* Enables experimental support for ES7 decorators. */
+    // "emitDecoratorMetadata": true,         /* Enables experimental support for emitting type metadata for decorators. */
+  },
+  "include": [
+    "**/*"
+  ],
+  "exclude": [
+    "./dist",
+    "./templates"
+  ]
+}`;
